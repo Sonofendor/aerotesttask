4001a380858b9db77f9d6e320369c05b8ab847dd
Artyom Grishechko <48153021+Sonofendor@users.noreply.github.com>
2022-07-18 04:26:38 +0300

    Fix errors tracking

--- load_data.py
+++ load_data.py
@@ -8,13 +8,22 @@ def log(level, message):
     print(strftime('%Y-%m-%d %H:%M:%S ') + levels[level] + ': ' + message)
 
 def getDataFromAPI(api_url):
+    global err_cnt
     log(0, 'Requesting data from API: {}'.format(api_url))
-    response = r.get(api_url)
-    data = response.json()
-    log(0, 'Successfully got data from API')
-    return data 
+    try:
+        response = r.get(api_url)
+        if response.status_code != 200:
+            raise ValueError(response.text)
+        data = response.json()
+        log(0, 'Successfully got data from API')
+        return data['stats']
+    except Exception as e:
+        log(2, 'Unable to get data from API: {}'.format(e))
+        err_cnt += 1
+        return None
     
 def loadStatsSingleSeasonData(data, cursor):
+    global err_cnt
     log(0, 'Loading statsSingleSeason...')
     try:
         gametype_values = [data['type']['gameType']['id'], 
@@ -29,12 +38,13 @@ def loadStatsSingleSeasonData(data, cursor):
 
             cursor.execute('''insert into stg."statsSingleSeason" values %s''', (insert_values,))
         
-        log(0, 'statsSingleSeason loaded successfully')
+        log(0, 'statsSingleSeason load complete')
     except Exception as e:
         log(2, 'Unable to load statsSingleSeason due to {}'.format(e))
         err_cnt += 1
         
 def loadRegularSeasonStatRankingsData(data, cursor):
+    global err_cnt
     log(0, 'Loading regularSeasonStatRankings...')
     try:
         for team in data['splits']:
@@ -45,12 +55,13 @@ def loadRegularSeasonStatRankingsData(data, cursor):
 
             cursor.execute('''insert into stg."regularSeasonStatRankings" values %s''', (insert_values,))
         
-        log(0, 'regularSeasonStatRankings loaded successfully')
+        log(0, 'regularSeasonStatRankings load complete')
     except Exception as e:
         log(2, 'Unable to load statsSingleSeason due to {}'.format(e))
         err_cnt += 1
         
 def loadDataToDB(data):
+    global err_cnt
     log(0, 'Connecting to DB {}:{}/{} as {}'.format(host, port, dbname, user))
     try:
         conn = connect(host=host, port=port, dbname=dbname, user=user, password=password)
@@ -62,16 +73,19 @@ def loadDataToDB(data):
                 loadRegularSeasonStatRankingsData(stats_record, cur)
             else:
                 log(1, 'Unknown stats type: {}. Skipping...'.format(stats_record['type']['displayName']))
+        if err_cnt != 0:
+            raise ValueError('Error while loading data, all transactions rolled back')
         conn.commit()
         conn.close()
     except Exception as e:
-        log(2, 'Unable to connect to DB due to {}'.format(e))
+        log(2, 'Unable to load data to DB due to {}'.format(e))
         err_cnt += 1
 
 if __name__ == '__main__':
     err_cnt = 0
     log(0, 'Pipeline started')
-    data = getDataFromAPI(api_url)['stats']
-    loadDataToDB(data)
+    data = getDataFromAPI(api_url)
+    if err_cnt == 0:
+        loadDataToDB(data)
     pipeline_status = 'successfully' if err_cnt == 0 else 'with errors'
     log(0, 'Pipeline finished ' + pipeline_status)
